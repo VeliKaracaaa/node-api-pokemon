22ae08fb58c153db980b9f2cf1b25d0cfbca04c5
veli <veli.karaca@epitech.eu>
2023-04-11 15:21:18 +0200

    New endpoint nb total pokemon in pokedex

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const express = require('express')
+let pokemons = require('./mock-pokemon')
 
 const app = express()
 const port = 3000
 app.get('/', (req, res) => res.send('Hello, Express !'))
 app.get('/api/pokemons/:id', (req, res) => {
-    const id = req.params.id
-    res.send(`Vous avez demandé le pokemon n°${id}`)
+    const id = parseInt(req.params.id)
+    const pokemon = pokemons.find(pokemon => pokemon.id === id)
+    res.send(`Vous avez demandé le pokemon n°${pokemon.name}`)
+})
+
+app.get('/api/pokemons', (req, res) => {
+    res.send(`Il y a ${pokemons.length} pokémons dans le pokédex pour le moment`)
 })
 app.listen(port, () => console.log(`Notre application Node est démarrée sur : http://localhost:${port}`))
