53c3da0b1ac538d7eae61b97af0dfe39069055a6
veli <veli.karaca@epitech.eu>
2023-04-26 13:00:42 +0200

    Gestion d'erreur

--- app.js
+++ app.js
@@ -19,4 +19,10 @@ require('./src/routes/findPokemonByPk')(app)
 require('./src/routes/createPokemon')(app)
 require('./src/routes/updatePokemon')(app)
 require('./src/routes/deletePokemon')(app)
+
+// Gestion d'erreur 404
+app.use(({res}) => {
+    const message = 'Impossible de trouver la ressource demandée ! Vous pouvez essayer une autre URL.'
+    res.status(404).json({message})
+})
 app.listen(port, () => console.log(`Notre application Node est démarrée sur : http://localhost:${port}`))
--- src/routes/findAllPokemons.js
+++ src/routes/findAllPokemons.js
@@ -3,9 +3,13 @@ const { Pokemon } = require('../db/sequelize')
 module.exports = (app) => {
     app.get('/api/pokemons', (req, res) => {
         Pokemon.findAll()
-            .then(pokemons => {
+            .then(pokemon => {
                 const message = 'La liste des pokémons a bien été récupérée.'
-                res.json({ message, data: pokemons })
+                res.json({ message, data: pokemon })
+            })
+            .catch(error => {
+                const message =  `La liste des pokémons n'a pas pu être récupérée. Réssayer dans quelque instants`
+                res.status(500).json({message, data: error})
             })
     })
 }
